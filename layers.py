0799c9b8bacb73df4cf2567d636ee040fb6ca3de
wuyuanhang94 <764591983@qq.com>
2020-06-23 09:46:45 +0800

    add conv and pooling layer

--- layers.py
+++ layers.py
@@ -1,6 +1,5 @@
 import numpy as np
-from common import softmax
-from common import crossEntropyLoss
+from common import *
 
 class ReLU:
     def __init__(self):
@@ -54,6 +53,94 @@ class SoftmaxWithLoss:
         self.dy *= dout
         return self.dy
 
+class Convolution:
+    def __init__(self, W, h, stride=1, pad=0):
+        self.W = W
+        self.h = h
+        self.stride = stride
+        self.pad = pad
+
+        self.x = None
+        self.col = None
+        self.col_W = None
+
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad) # X 二维
+        col_W = self.W.reshape(FN, -1).T # W 二维
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col # x -> col 反向传播要用到
+        self.col_W = col_W # W -> col_W 反向传播要用到
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0, 2, 3, 1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+        self.x = None
+        self.arg_max = None # 需要记下max的坐标
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        arg_max = np.arg_max(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        # 池化层反响传播的难点在于 其本质是不可导的
+        # 压缩尺寸后的梯度传递是不对位的
+        # 解决办法就是arg_max处有梯度, 其他idx为0
+        dout = dout.transpose(0, 2, 3, 1)
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+        
+        return dx
 
 if __name__ == "__main__":
     x = np.array([[-1, 2, -2], [0, .2, 3]])
