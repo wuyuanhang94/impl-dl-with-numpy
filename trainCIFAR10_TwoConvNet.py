63276d670ae9f55f8546a3d64a2141576c8bdbf8
Yi Wu <yiw@nvidia.com>
2020-06-24 08:28:00 +0800

    fix bug

--- trainCIFAR10_TwoConvNet.py
+++ trainCIFAR10_TwoConvNet.py
@@ -6,12 +6,12 @@ from trainer import Trainer
 (x_train, t_train), (x_test, t_test) = load_cifar10(flatten=False)
 # print(x_train.shape)
 network = ConvNet(input_dim=(3,32,32), 
-                  conv_param1={'filter num': 50, 'filter size':5, 'pad':1, 'stride':1},
-                  conv_param2={'filter num': 20, 'filter size':3, 'pad':1, 'stride':1},
+                  conv_param1={'filter num': 64, 'filter size':3, 'pad':1, 'stride':1},
+                  conv_param2={'filter num': 64, 'filter size':3, 'pad':1, 'stride':1},
                   output_size=10, weight_init_std=0.01, pretrained=False)
                         
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
-                  epochs=10, mini_batch_size=128,
-                  optimizer='AdaGrad', optimizer_param={'lr': 0.005})
+                  epochs=20, mini_batch_size=128,
+                  optimizer='AdaGrad', optimizer_param={'lr': 0.001})
 
 trainer.train()
